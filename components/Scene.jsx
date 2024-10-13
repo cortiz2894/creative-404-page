aec7b4d0a1cd4be803dd1d691921ae6849cfadf0
cortiz2894 <git@stackblitz.com>
2024-10-13 20:13:57 -0300

    Initial commit

---
+++ components/Scene.jsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { Canvas } from '@react-three/fiber';
+import Model from './Model';
+import { Environment } from '@react-three/drei';
+import { useControls } from 'leva';
+import styles from './Scene.module.scss';
+import { Leva } from 'leva';
+
+export default function Scene() {
+  const lightProps = useControls({
+    intensity: { value: 1.35, min: 0, max: 10, step: 0.05 },
+    positionY: { value: 0, min: -10, max: 10, step: 0.1 },
+    positionX: { value: -0.3, min: -10, max: 10, step: 0.1 },
+    positionZ: { value: -1.9, min: -10, max: 10, step: 0.1 },
+  });
+
+  return (
+    <>
+      <Leva collapsed />
+      <div className={styles.blurElement}></div>
+      <Canvas style={{ backgroundColor: 'transparent' }}>
+        <directionalLight
+          intensity={lightProps.intensity}
+          position={[
+            lightProps.positionY,
+            lightProps.positionX,
+            lightProps.positionZ,
+          ]}
+        />
+        <Environment preset="city" />
+        <Model />
+      </Canvas>
+    </>
+  );
+}
